58a45f2df74d7c0910dc3342aa2bf30fad3c16bb
hatlonely <hatlonely@gmail.com>
2022-04-09 18:23:18 +0800

    update

--- ben/framework/stop.py
+++ ben/framework/stop.py
@@ -21,15 +21,19 @@ class Stop(object):
     def next(self):
         if self.seconds != 0 and (datetime.now() - self.now).total_seconds() > self.seconds:
             return False
+        if self.times == 0:
+            return True
         # 有性能瓶颈，mac 上测试，一秒仅能执行 6w 次，且多个 atomic 变量看起来会串行执行
         val = self.count.fetch_inc()
-        if self.times != 0 and val >= self.times:
+        if val >= self.times:
             return False
         return True
 
     def is_running(self):
         if self.seconds != 0 and (datetime.now() - self.now).total_seconds() > self.seconds:
             return False
-        if self.times != 0 and self.count.load() >= self.times:
+        if self.times == 0:
+            return True
+        if self.count.load() >= self.times:
             return False
         return True
